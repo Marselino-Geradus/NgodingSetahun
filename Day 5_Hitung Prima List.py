1a842063531a1a93477a60750c2523b46030a57f
Marselino_Geradus <marselino914@gmail.com>
2024-01-05 23:54:09 +0800

    Hitung Jumlah Bilangan Prima dalam Sebuah List

---
+++ Day 5_Hitung Prima List.py
@@ -0,0 +1,18 @@
+a = range(1,10)
+print('Deretan bilangan = ',list(a))
+print('='*5)
+
+jumlah_prima = 0
+def prima(a):
+    if a <= 1:
+        return False
+    for i in range(2,int(a**0.5)+1):
+        if a % i == 0:
+            return False
+    return True
+    
+#hitung bil prima
+for angka in list(a):
+    if prima(angka):
+        jumlah_prima += 1
+print(jumlah_prima)
